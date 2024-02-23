ab2fd0c5a6b8407b249758d03fbb2f89e52fb63a
Mauricio Quintero <mauri1987@gmail.com>
2024-02-23 20:29:52 -0300

    4-Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [CommonModule, FormsModule, HeroDetailComponent],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes: Hero[] = HEROES;
+  // this is other way to inject dependency
+  //private heroService = inject(HeroService)
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  messageService = inject(MessageService);
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+    //this.heroes = this.heroService.getHeroes();
+  }
 
   buttonClicked(hero: Hero) {
+    this.messageService.add(`${hero.name} clicked`);
     if (this.selectedHero === hero)
       this.selectedHero = undefined;
     else
